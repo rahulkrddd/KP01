d639aca43c9dc3cb09c8f09a4e7cc00a81b0403b
rahulkrddd <rahul.gupta.730@gmail.com>
2024-08-15 21:22:31 +0530

    Update displaySearchResults.js

--- public/displaySearchResults.js
+++ public/displaySearchResults.js
@@ -441,6 +441,32 @@ function showPopup(encodedUri) {
     messageElement.style.lineHeight = '1.5';
     popup.appendChild(messageElement);
 
+    // Create the open File button
+    const openLinkButton = document.createElement('button');
+    openLinkButton.innerText = 'Open File';
+    openLinkButton.style.padding = '12px 24px';
+    openLinkButton.style.border = 'none';
+    openLinkButton.style.borderRadius = '8px';
+    openLinkButton.style.backgroundColor = '#28a745'; // Green color for the button
+    openLinkButton.style.color = '#fff';
+    openLinkButton.style.cursor = 'pointer';
+    openLinkButton.style.fontSize = '16px';
+    openLinkButton.style.fontWeight = 'bold';
+    openLinkButton.style.transition = 'background-color 0.3s';
+    openLinkButton.style.marginBottom = '10px'; // Add space between buttons
+    openLinkButton.addEventListener('click', () => {
+        window.open(encodedUri, '_blank'); // Open the copied link in a new tab
+    });
+    openLinkButton.addEventListener('mouseover', () => {
+        openLinkButton.style.backgroundColor = '#218838'; // Darker green shade on hover
+    });
+    openLinkButton.addEventListener('mouseout', () => {
+        openLinkButton.style.backgroundColor = '#28a745'; // Original color
+    });
+	openLinkButton.style.marginRight = '20px'; // Add margin to the right of the openLinkButton
+    popup.appendChild(openLinkButton);
+	
+
     // Create the close button
     const closeButton = document.createElement('button');
     closeButton.innerText = 'Close';
@@ -476,6 +502,7 @@ function showPopup(encodedUri) {
         console.error('Could not copy text: ', err);
     });
 }
+
 
 
 
